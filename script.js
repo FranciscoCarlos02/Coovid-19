163adb19bff761c7b350353a6fb9c7f6950592d5
FranciscoCarlos02 <101338809+FranciscoCarlos02@users.noreply.github.com>
2022-04-06 15:25:19 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,123 @@
+function aparecerTexto(){
+    let valor = 1
+
+    switch(valor){
+        case 1: {
+            let div = document.getElementById("texto");
+            div.classList.remove("inneu");
+            div.classList.add("inner");
+            document.getElementById("texto").innerHTML = "Fique em casa, peça para fazerem as compras por voce!";
+        }
+        case 2:{
+            let div = document.getElementById("texto");
+            div.classList.remove("inneu");
+            div.classList.add("inner");
+            document.getElementById("texto").innerHTML = "Fique em casa, peça para fazerem as compras por voce!";
+        }
+        case 3: {
+            let div = document.getElementById("texto");
+            div.classList.remove("inneu");
+            div.classList.add("inner");
+            document.getElementById("texto").innerHTML = "Fique em casa, peça para fazerem as compras por voce!";
+        }
+        case 4:{
+            let div = document.getElementById("texto");
+            div.classList.remove("inneu");
+            div.classList.add("inner");
+            document.getElementById("texto").innerHTML = "Fique em casa, peça para fazerem as compras por voce!";
+        }
+    }
+}
+function aparecerTexto2(){
+    let valor = 1
+
+    switch(valor){
+        case 1: {
+            let div = document.getElementById("texto");
+            div.classList.remove("inneu");
+            div.classList.add("inner");
+            document.getElementById("texto").innerHTML = "Fique em casa. Tome sol e faça exercicios leves!";
+        }
+        case 2:{
+            let div = document.getElementById("texto");
+            div.classList.remove("inneu");
+            div.classList.add("inner");
+            document.getElementById("texto").innerHTML = "Fique em casa. Tome sol e faça exercicios leves!";
+        }
+        case 3: {
+            let div = document.getElementById("texto");
+            div.classList.remove("inneu");
+            div.classList.add("inner");
+            document.getElementById("texto").innerHTML = "Fique em casa. Tome sol e faça exercicios leves!";
+        }
+        case 4:{
+            let div = document.getElementById("texto");
+            div.classList.remove("inneu");
+            div.classList.add("inner");
+            document.getElementById("texto").innerHTML = "Fique em casa. Tome sol e faça exercicios leves!";
+        }
+    }
+}
+
+function aparecerTexto3(){
+    let valor = 1
+
+    switch(valor){
+        case 1: {
+            let div = document.getElementById("texto");
+            div.classList.remove("inneu");
+            div.classList.add("inner");
+            document.getElementById("texto").innerHTML = "Fique em casa, e cuide de sua saúde!";
+        }
+        case 2:{
+            let div = document.getElementById("texto");
+            div.classList.remove("inneu");
+            div.classList.add("inner");
+            document.getElementById("texto").innerHTML = "Fique em casa, e cuide de sua saúde!";
+        }
+        case 3: {
+            let div = document.getElementById("texto");
+            div.classList.remove("inneu");
+            div.classList.add("inner");
+            document.getElementById("texto").innerHTML = "Fique em casa, e cuide de sua saúde!";
+        }
+        case 4:{
+            let div = document.getElementById("texto");
+            div.classList.remove("inneu");
+            div.classList.add("inner");
+            document.getElementById("texto").innerHTML = "Fique em casa, e cuide de sua saúde!";
+        }
+    }
+}
+
+function aparecerTexto4(){
+    let valor = 1
+
+    switch(valor){
+        case 1: {
+            let div = document.getElementById("texto");
+            div.classList.remove("inneu");
+            div.classList.add("inner");
+            document.getElementById("texto").innerHTML =  "Faça compras pelos idosos, mas use máscara!"
+        }
+        case 2:{
+            let div = document.getElementById("texto");
+            div.classList.remove("inneu");
+            div.classList.add("inner");
+            document.getElementById("texto").innerHTML = "Faça compras pelos idosos, mas use máscara!";
+        }
+        case 3: {
+            let div = document.getElementById("texto");
+            div.classList.remove("inneu");
+            div.classList.add("inner");
+            document.getElementById("texto").innerHTML = "Faça compras pelos idosos, mas use máscara!";
+        }
+        case 4:{
+            let div = document.getElementById("texto");
+            div.classList.remove("inneu");
+            div.classList.add("inner");
+            document.getElementById("texto").innerHTML = "Faça compras pelos idosos, mas use máscara!";
+        }
+    }
+}
+
